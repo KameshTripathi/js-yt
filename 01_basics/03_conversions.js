c786dc2a6351d32cc6a7e3150e7804b8904948a0
KameshTripathi <120265582+KameshTripathi@users.noreply.github.com>
2024-02-18 13:39:17 +0000

    conversion of data types

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,51 @@
+let score = "33"
+let str = "33abc"
+let variable = null
+let score2 = undefined
+let score3 = true
+let score4 = false
+let isLoggedIn = 1
+
+console.log(typeof score);
+console.log(typeof(str));
+console.log(typeof variable);
+console.log(typeof score2)
+console.log(typeof score3)
+console.log(typeof score4)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let valueInNum = Number(str)
+console.log(typeof valueInNum);
+console.log(valueInNum)
+
+let valueInN = Number(variable)
+console.log(typeof valueInN)
+console.log(valueInN)
+
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+
+let valueInNumber3 = Number(score3)
+console.log(typeof valueInNumber3)
+console.log(valueInNumber3)
+
+let valueInNumber4 = Number(score4)
+console.log(typeof valueInNumber4)
+console.log(valueInNumber4)
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn)
+// Try assigning 0, "kamesh", "" to isLoggedIn.
+// 0 converts to false, non-empty string converts to true, empty string converts to false.
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
